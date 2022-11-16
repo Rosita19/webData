e4cca65ed0a6adb18490eb80a210fec2c5e1512d
Rosita Dewi Lutfiyah <83932539+Rosita19@users.noreply.github.com>
2022-11-17 01:34:44 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 st.title("""
-PERHITUNGAN DATA
+Data Pasien Menderitta Stroke
 """)
 
 test = st.sidebar.radio("Menu", ['Learn Data', 'Preprocessing', 'Model', 'Implementasi'])
@@ -12,20 +12,5 @@ test = st.sidebar.radio("Menu", ['Learn Data', 'Preprocessing', 'Model', 'Implem
 if test == "Learn Data":
    df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
    df
-
-#Fractional Knapsack Problem
-#Getting input from user
-kodekontrak=int(st.number_input("Kode Kontrak: ",0))
-Pendapat=int(st.number_input("Pendapatan Setahun : ",0))
-durasipinjaman=int(st.number_input("Durasi Pinjaman : ",0))
-jumlahtanggungan=int(st.number_input("Jumlah Tanggungan : ",0))
-kpr=str(st.text_input("KPR : ",'ya'))
-ovd=str(st.text_input("Rata-Rata Overdue : ",'ya'))
-
-submit = st.button("submit")
-
-
-if submit:
-    st.info("Jadi,dinyakataan . ")
 
 
