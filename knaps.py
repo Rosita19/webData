cc51aca5014c1f1f76275187c833183e7b14c10c
Rosita Dewi Lutfiyah <83932539+Rosita19@users.noreply.github.com>
2022-11-17 02:05:59 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,114 +1,68 @@
-import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from PIL import Image
+import re
+import pandas as pd 
+import numpy as np
 
-
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-
-# Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("style.css")
+local_css("style/style.css")
 
-# ---- LOAD ASSETS ----
-lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
-img_contact_form = Image.open("images/yt_contact_form.png")
-img_lottie_animation = Image.open("images/yt_lottie_animation.png")
+test = st.sidebar.radio("Menu", ['Learn Data', 'Preprocessing', 'Model', 'Implementasi'])
 
-# ---- HEADER SECTION ----
-with st.container():
-    st.subheader("Hi, I am Sven :wave:")
-    st.title("A Data Analyst From Germany")
-    st.write(
-        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
-    )
-    st.write("[Learn More >](https://pythonandvba.com)")
+if test == "Learn Data":
+   st.title("""
+      Data Pasien Menderita Stroke
+      """)
+   df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
+   df
+elif test == "Preprocessing":
+   st.title("""
+      Pemrosesan Data
+      """)
+   df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
+   df
+   kodekontrak=int(st.number_input("Kode Kontrak: ",0))
+   Pendapat=int(st.number_input("Pendapatan Setahun : ",0))
+   durasipinjaman=int(st.number_input("Durasi Pinjaman : ",0))
+   jumlahtanggungan=int(st.number_input("Jumlah Tanggungan : ",0))
+   kpr=str(st.text_input("KPR : ",'ya'))
+   ovd=str(st.text_input("Rata-Rata Overdue : ",'ya'))
 
-# ---- WHAT I DO ----
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("What I do")
-        st.write("##")
-        st.write(
-            """
-            On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way."
+   submit = st.button("submit")
+   
+   if submit:
+       st.info("Jadi,dinyakataan . ")
 
-            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
-            """
-        )
-        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
-    with right_column:
-        st_lottie(lottie_coding, height=300, key="coding")
+elif test == "Model":
+   st.title("""
+      Modeling 
+      """)
+   from sklearn.neighbors import KNeighborsClassifier
+   from numpy import array
+   
+   menu = st.sidebar.radio("Pilihan", ['KNN', 'Gaussian Naive Bayes', 'Decision Tree'])
 
-# ---- PROJECTS ----
-with st.container():
-    st.write("---")
-    st.header("My Projects")
-    st.write("##")
-    image_column, text_column = st.columns((1, 2))
-    with image_column:
-        st.image(img_lottie_animation)
-    with text_column:
-        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
-        st.write(
-            """
-            Learn how to use Lottie Files in Streamlit!
-            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
-            In this tutorial, I'll show you exactly how to do it
-            """
-        )
-        st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
-with st.container():
-    image_column, text_column = st.columns((1, 2))
-    with image_column:
-        st.image(img_contact_form)
-    with text_column:
-        st.subheader("How To Add A Contact Form To Your Streamlit App")
-        st.write(
-            """
-            Want to add a contact form to your Streamlit website?
-            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
-            """
-        )
-        st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
+   if test == "KNN":
+    st.title("""
+        KNN (K-Nearest Neighbor)
+        """)
+    df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
+    df
+    
+    metode1 = KNeighborsClassifier(n_neighbors=3)
+    metode1.fit(X_train, y_train)
+    print(metode1.score(X_train, y_train))
+    print(metode1.score(X_test, y_test))
+    y_pred = metode1.predict(scaler.transform(array([[50.0,0,1,105.92,0,0,1,0,1,0,1,1,1,1,1,1,1,0,0,0,0]])))
+    le.inverse_transform(y_pred)[0]
 
-# ---- CONTACT ----
-with st.container():
-    st.write("---")
-    st.header("Get In Touch With Me!")
-    st.write("##")
+elif test == "Implementasi":
+   st.title("""
+      Implementasi Data
+      """)
 
-    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-    contact_form = """
-    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
-        <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Your name" required>
-        <input type="email" name="email" placeholder="Your email" required>
-        <textarea name="message" placeholder="Your message here" required></textarea>
-        <button type="submit">Send</button>
-    </form>
-    """
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.markdown(contact_form, unsafe_allow_html=True)
-    with right_column:
-        st.empty()
+
+
