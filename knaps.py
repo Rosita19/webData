5c7391a35d6251913408350e25a08d7d8b256fc6
Rosita Dewi Lutfiyah <83932539+Rosita19@users.noreply.github.com>
2022-11-17 01:35:29 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -3,13 +3,12 @@ import re
 import pandas as pd 
 import numpy as np
 
-st.title("""
-Data Pasien Menderitta Stroke
-""")
-
 test = st.sidebar.radio("Menu", ['Learn Data', 'Preprocessing', 'Model', 'Implementasi'])
 
 if test == "Learn Data":
+   st.title("""
+      Data Pasien Menderita Stroke
+      """)
    df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
    df
 
