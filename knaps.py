8b5127f58f2712b56301bf65d8f65b4a20a336b1
Rosita Dewi Lutfiyah <83932539+Rosita19@users.noreply.github.com>
2022-11-17 01:32:13 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -10,35 +10,8 @@ PERHITUNGAN DATA
 test = st.sidebar.radio("Menu", ['Learn Data', 'Preprocessing', 'Model', 'Implementasi'])
 
 if test == "Learn Data":
-    st.subheader("Hai, saya Jumadi :wave:")
-    st.title("Saya Seorang coach artificial intelligence dari Indonesia")
-    st.write(
-        "Saya bersemang untuk menggunakan Streamlit lebih efisien dan efektif dalam bisnis."
-    )
-    st.write("[Pelajari Lebih Lanjut](https://kelasawanpintar.netlify.app/)")
-
-    st.write("---")
-
-    st.header("Apa yang saya lakukan")
-    st.write("##")
-    st.write(
-            """
-            Di Channel YouTube saya, saya membuat tutorial untuk orang-orang yang:
-            - sedang mencari cara untuk belajar Python.
-            - sedang mencari cara untuk belajar Streamlit.
-            - ingin belajar Analisis Data & Ilmu Data untuk melakukan analisis.
-            - ingin belajar Artificial Intelligence, Data Science, Machine Learning, Natural Language Processing.
-            - ingin belajar dunia IT
-            - Jika ingin terhubung di [Linkedin](https://www.linkedin.com/in/jumadi-01/)
-
-
-            Jika Channel YouTube saya menarik bagi Anda, jangan lupa untuk berlangganan dan menyalakan notifikasi, agar Anda tidak ketinggalan konten apa pun.
-            
-            [Channel YouTube](https://www.youtube.com/channel/UC7rCdlKnMTt26Q3np3rW1Iw)
-            """
-        )
-    st.header("Playlist Fundamental Streamlit")
-    col1, col2, col3 = st.columns(3)
+   df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
+   df
 
 #Fractional Knapsack Problem
 #Getting input from user
