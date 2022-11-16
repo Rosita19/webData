630c135a970c4e916508155c4c7c27615a83cd6a
Rosita Dewi Lutfiyah <83932539+Rosita19@users.noreply.github.com>
2022-11-17 01:40:41 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -11,5 +11,33 @@ if test == "Learn Data":
       """)
    df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
    df
+elif test == "Preprocessing":
+   st.title("""
+      Pemrosesan Data
+      """)
+   df = pd.read_csv("https://raw.githubusercontent.com/Rosita19/datamining/main/healthcare-dataset-stroke-data.csv")
+   df
+   kodekontrak=int(st.number_input("Kode Kontrak: ",0))
+   Pendapat=int(st.number_input("Pendapatan Setahun : ",0))
+   durasipinjaman=int(st.number_input("Durasi Pinjaman : ",0))
+   jumlahtanggungan=int(st.number_input("Jumlah Tanggungan : ",0))
+   kpr=str(st.text_input("KPR : ",'ya'))
+   ovd=str(st.text_input("Rata-Rata Overdue : ",'ya'))
+
+   submit = st.button("submit")
+   
+   if submit:
+       st.info("Jadi,dinyakataan . ")
+
+elif test == "Model":
+   st.title("""
+      Modeling 
+      """)
+
+elif test == "Implementasi":
+   st.title("""
+      Implementasi Data
+      """)
+
 
 
